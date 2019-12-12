e94fc5522b494ac178f9a9e2cbee9c6a303b7d23
Ozal Khan <ozalkhan196@gmail.com>
2019-12-11 21:42:11 -0500

    added nav, book list and context folder

---
+++ src/components/BookList.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const BookList = () => {
+  return (
+    <div className="book-list">
+      <ul>
+        <li>the art of war</li>
+        <li>mystic river</li>
+        <li>Harry Potter</li>
+      </ul>
+    </div>
+  );
+};
+
+export default BookList;
